90f71e51aef8e8fea050622f1684dd2106074bcb
1henri1 <henri135246@gmail.com>
2023-06-01 21:57:27 -0300

    java feito

--- app/src/main/java/br/com/etecia/merecicla/LoginSelect_Activity.java
+++ app/src/main/java/br/com/etecia/merecicla/LoginSelect_Activity.java
@@ -2,13 +2,38 @@ package br.com.etecia.merecicla;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class LoginSelect_Activity extends AppCompatActivity {
+import com.google.android.material.card.MaterialCardView;
+
+public class LoginSelect_Activity extends AppCompatActivity  {
+
+    MaterialCardView usu, col;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loginselect_layout);
+
+        usu = findViewById(R.id.usu);
+        col = findViewById(R.id.col);
+
+        usu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Login_Activity.class));
+                finish();
+            }
+        });
+        col.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Login_Activity.class));
+                finish();
+            }
+        });
+
     }
-}
+    }
--- app/src/main/java/br/com/etecia/merecicla/Login_Activity.java
+++ app/src/main/java/br/com/etecia/merecicla/Login_Activity.java
@@ -2,13 +2,37 @@ package br.com.etecia.merecicla;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login_Activity extends AppCompatActivity {
+
+    Button logar,voltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+        logar = findViewById(R.id.logar);
+        voltar = findViewById(R.id.voltar);
+
+        logar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Menu_Activity.class));
+                finish();
+            }
+        });
+
+        voltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),LoginSelect_Activity.class));
+                finish();
+            }
+        });
     }
 }
